37df0e1bb80054c6dd192cc2192d138b403e246f
iviblop <67535696+iviblop@users.noreply.github.com>
2021-11-07 21:32:29 +0900

    Update question_2.py

--- question_2.py
+++ question_2.py
@@ -1,25 +1,23 @@
-def Sum(foo):
-    global SUM
-    print(foo)
-    SUM += foo
-
-
 def main():
+    SUM = 0
     while True:
         foo = input().replace(" ", "")
         try:
             if '의배수' in foo:
                 for bar in range(int(foo[0]), 101, int(foo[0])):
-                    Sum(bar)
+                    print(foo)
+                    SUM += foo
                 break
             elif '수' in foo:
                 if '짝' in foo:
                     for bar in range(2, 101, 2):
-                        Sum(bar)
+                        print(foo)
+                        SUM += foo
                     break
                 elif '홀' in foo:
                     for bar in range(1, 101, 2):
-                        Sum(bar)
+                        print(foo)
+                        SUM += foo
                     break
             print("프로토콜에 어긋난 입력입니다.")
         except ValueError:
@@ -29,6 +27,5 @@ def main():
 
 
 if __name__ == '__main__':
-    SUM = 0
     main()
 
