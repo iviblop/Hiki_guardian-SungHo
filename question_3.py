b99335ca499edb0deb37f17ce99974cb47f34aed
iviblop <67535696+iviblop@users.noreply.github.com>
2021-11-07 21:45:28 +0900

    Update question_3.py

--- question_3.py
+++ question_3.py
@@ -1,12 +1,8 @@
-def Devide(a, b):
-    print(b / a)
-
-
 def main():
     while True:
         try:
             a, b = map(int, input().strip().split(", "))
-            Devide(a, b)
+            print(b / a)
             break
         except ValueError:
             print("프로토콜에 어긋난 입력입니다.")
