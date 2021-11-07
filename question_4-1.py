a7ad2d18a0dfb8329b4e8cb12f511a4585103e26
iviblop <67535696+iviblop@users.noreply.github.com>
2021-11-07 21:46:41 +0900

    Update question_4-1.py

--- question_4-1.py
+++ question_4-1.py
@@ -3,7 +3,7 @@ def main():
         try:
             foo = int(input().replace(" ", ""))
             for bar in range(1, 10):
-                print(f"{foo} * {bar} = {foo * bar}", sep='')
+                print(f"{foo} * {bar} = {foo * bar}")
             break
         except ValueError:
             print("프로토콜에 어긋난 입력입니다.")
